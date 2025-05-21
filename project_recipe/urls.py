6f92949a5db993029208432e076634f142639a14
dmitropariy <dmitropariy@gmail.com>
2025-05-21 14:33:59 +0300

    Add view main

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+import recipe.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', recipe.views.main, name='main'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,7 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
 
+def main(request):
+    recipes = Recipe.objects.order_by('-created_at')[:5]
+    context = { 'recipes': recipes }
+    return render(request, 'main.html', context)
